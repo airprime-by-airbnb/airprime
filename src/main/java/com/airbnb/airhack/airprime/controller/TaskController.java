641c73f6986515f0b2016364f1c227835e4cb19e
Reda Zouhairi <zouhairireda@gmail.com>
2019-07-12 22:41:45 +0200

    add request to submitTasks airhack

--- src/main/java/com/airbnb/airhack/airprime/controller/TaskController.java
+++ src/main/java/com/airbnb/airhack/airprime/controller/TaskController.java
@@ -1,7 +1,12 @@
 package com.airbnb.airhack.airprime.controller;
 
+import java.net.URISyntaxException;
 import java.time.LocalDateTime;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +14,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
 
 import com.airbnb.airhack.airprime.model.Batch;
 
@@ -20,20 +27,45 @@ import lombok.extern.slf4j.Slf4j;
 @CrossOrigin(origins = "*", methods = { RequestMethod.GET, RequestMethod.OPTIONS, RequestMethod.POST })
 public class TaskController {
 
+	@Autowired
+	private RestTemplate restTemplate;
+
 	@PostMapping(value = "/incomingTasks")
-	public Batch incomingTasks(@RequestBody Batch batch) {
+	public Batch incomingTasks(@RequestBody Batch batch) throws RestClientException, URISyntaxException {
 		log.info("------------------ " + LocalDateTime.now());
 		log.info("------------------ Inside incoming Tasks POST");
 		log.info("------------------ Batch : " + batch);
 
-		return batch;
+		Batch processedBatch = batch;
+		log.info("------------------ " + LocalDateTime.now());
+		log.info("------------------ Processing");
+
+		// TODO processing
+
+		log.info("------------------ " + LocalDateTime.now());
+		log.info("------------------ End of processing");
+
+		log.info("------------------ " + LocalDateTime.now());
+		log.info("------------------ Send request to /api/submitTasks");
+		// TODO send request
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		headers.set("Authorization", "Bearer " + "3IgAUjz5f3Rntu1WefSt6vTXZ5C7dgEi3cU75t4L2hJSCNLIkaXsDid4gdWl");
+
+		HttpEntity<Batch> entity = new HttpEntity<Batch>(batch, headers);
+		String result = restTemplate.postForObject("http://airhack-api.herokuapp.com/api/submitTasks", entity,
+				String.class);
+
+		System.out.println(result);
+		return processedBatch;
 	}
 
 	@GetMapping("/ping")
 	public String ping() {
 		log.info("------------------ " + LocalDateTime.now());
 		log.info("------------------ Inside ping");
-		
+
 		return "ping OK";
 	}
 }
